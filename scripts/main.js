f5c29b57e76dc571f3ef744297294d363372657c
PavloPantus <mr.pantus@gmail.com>
2019-11-28 12:02:38 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -21,18 +21,20 @@ const food = {
 
 const tree = document.querySelector('#tree');
 
-
 function createTree(element, data) {
   const list = document.createElement('ul');
+
   for (const key in data) {
     const li = document.createElement('li');
+
     li.append(key);
     list.append(li);
+
     if (Object.keys(data[key]).length > 0) {
       createTree(list, data[key]);
     }
   }
-    element.append(list);
+  element.append(list);
 }
 
 createTree(tree, food);
